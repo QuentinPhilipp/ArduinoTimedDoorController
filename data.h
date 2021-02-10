d9acbebab93910a87eaaed89981b91831f0935f9
Quentin Philipp <quentin.philipp@hotmail.fr>
2021-02-10 14:22:06 +0100

    Create data.h

---
+++ data.h
@@ -0,0 +1,34 @@
+// Define hour of closing
+// Format 00h00
+static const String calendarOff[] = {
+  "18h00",   //Januray
+  "19h00",   //February
+  "21h00",   //March
+  "21h00",   //April
+  "21h00",   //May
+  "22h00",   //June
+  "22h00",   //July
+  "22h00",   //August
+  "21h00",   //September
+  "21h00",   //October
+  "20h00",   //November
+  "18h00"   //December
+};
+
+
+// Define hour of opening
+// Format 00h00
+static const String calendarOn[] = {
+  "07h10",   //Januray
+  "07h10",   //February
+  "07h10",   //March
+  "07h10",   //April
+  "07h10",   //May
+  "07h10",   //June
+  "07h10",   //July
+  "07h10",   //August
+  "07h10",   //September
+  "07h10",   //October
+  "07h10",   //November
+  "07h10"    //December
+};
